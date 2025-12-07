816b610b157c76b4ebad84873e9c6ae65e27afa8
Diana Khizhnova <khnizovad@gmail.com>
2025-12-07 16:52:59 +0500

    feat: create new project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue';
+import Card from './components/Card.vue';
+
+const isAdded = ref(true)
+</script>
+
+<template>
+    <Card title="Puma" price="1200$" :isAdded="isAdded" />
+</template>
+
+<style scoped></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+interface Props {
+    title: string;
+    price: string;
+    isAdded: boolean
+}
+
+const props = defineProps<Props>()
+console.log(props)
+</script>
+
+<template>
+    <div class="card">
+        <p class="title">{{ props.title }}</p>
+        <p class="title">{{ props.price }}</p>
+        <p class="title">{{ props.isAdded }}</p>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.title {
+    font-size: 12px;
+}
+</style>
